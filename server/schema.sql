49e1c273d5f06a33ffe50819bc6b5d7f0f137c02
Meyvi <meyvim@gmail.com>
2020-11-18 18:43:57 -0800

    Changes id in answer schema

--- server/schema.sql
+++ server/schema.sql
@@ -17,13 +17,13 @@ CREATE TABLE questions (
 );
 
 CREATE TABLE answers (
-  id INT NOT NULL AUTO_INCREMENT,
+  answer_id INT NOT NULL AUTO_INCREMENT,
   answer_date DATE NOT NULL,
   answer TEXT NOT NULL,
   question_id INT NOT NULL,
   answer_flag INT,
   answer_helpful INT,
-  PRIMARY KEY (id),
+  PRIMARY KEY (answer_id),
   FOREIGN KEY (question_id) REFERENCES questions(id)
 );
 
