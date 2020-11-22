3bb9c4869c16eee5358c11251fc0f699e55f8c2d
Meyvi <meyvim@gmail.com>
2020-11-22 12:11:31 -0800

    Removes unused const in server

--- server/index.js
+++ server/index.js
@@ -4,23 +4,10 @@ const cors = require('cors');
 
 const app = express();
 const port = 3004;
-<<<<<<< HEAD
-=======
 const getQsAndAs = require('../db/index.js');
->>>>>>> sever-and-api
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'client/dist')));
-<<<<<<< HEAD
-
-app.get('/questions', cors(corsOptions), (req, res) => {
-  console.log(res);
-
-  res.json('get request');
-});
-
-app.listen(port, () => {
-=======
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
@@ -33,7 +20,6 @@ app.get('/product/:product_id/q-and-a', (req, res) => {
 });
 
 app.post('/product/:product_id/helpful', (req, res) => {
-  const productId = req.params.product_id;
   const answerIdToChange = req.body.answerId;
 
   getQsAndAs.handleHelpful(answerIdToChange, () => {
@@ -44,6 +30,5 @@ app.post('/product/:product_id/helpful', (req, res) => {
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
->>>>>>> sever-and-api
   console.log(`Listening on port ${port}`);
 });
